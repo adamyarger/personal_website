a1f59d82cf3e45226817a08ba6852b959d220da1
adamyarger <yargeradam@gmail.com>
2015-06-04 08:51:10 -0500

    adds new video view

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,10 +2,32 @@ class VideosController < ApplicationController
 	before_action :authenticate_user!, only: :create
 
 	def index
-		
+		@videos = Video.all
 	end
 
 	def show
-		
+		@video = Video.find(params[:id])
 	end
+
+	def create
+		@video = current_user.videos.create(video_params)
+
+		if @video.save
+			flash[:success] = 'video posted'
+			redirect_to @video
+		else
+			flash[:error] = 'Somthing went wrong'
+			render :new
+		end
+	end
+
+	def new
+		@video = Video.new
+	end
+
+	private
+
+		def video_params
+			params.require(:video).permit(:title, :content, :description)
+		end
 end
