bd3a88c870db208f0ff0efc72ed3e4555ec601e5
adamyarger <yargeradam@gmail.com>
2015-06-20 11:51:43 -0500

    fixes turbolink problem

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,4 +1,5 @@
-jQuery(document).ready(function($){
+$(document).ready(function(){
+
   //if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
   var MQL = 1170;
 
